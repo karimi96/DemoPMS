b12ad7783a9c2e37db49337eefce48c18a740763
karimi <zmanyamyd5@gmail.com>
2022-07-01 17:21:09 +0430

    Add barcode reader feature to project

---
+++ app/src/main/java/com/karimi/googlemap/activity/barcodrScanner/KtApplication.kt
@@ -0,0 +1,12 @@
+package com.karimi.googlemap.activity
+
+import androidx.multidex.MultiDexApplication
+
+//import android.support.multidex.MultiDexApplication
+
+class KtApplication: MultiDexApplication(){
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
