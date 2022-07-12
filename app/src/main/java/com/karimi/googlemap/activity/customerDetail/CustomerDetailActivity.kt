e5aed159613ceaa0477ae0a73fac40a98f192284
karimi <zmanyamyd5@gmail.com>
2022-07-12 15:47:42 +0430

    Fix listener For WebView

--- app/src/main/java/com/karimi/googlemap/activity/customerDetail/CustomerDetailActivity.kt
+++ app/src/main/java/com/karimi/googlemap/activity/customerDetail/CustomerDetailActivity.kt
@@ -5,16 +5,17 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.TextView
-import android.widget.TextView.OnEditorActionListener
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import com.karimi.googlemap.R
@@ -24,17 +25,18 @@ import com.karimi.googlemap.database.dao.CustomerDao
 import com.karimi.googlemap.model.Customer
 import com.karimi.googlemap.sqlite.DBHelperJavaSimin
 import kotlinx.android.synthetic.main.activity_customer_detaile.*
-import kotlinx.android.synthetic.main.activity_customer_detaile.et_TerminalNumber
-import kotlinx.android.synthetic.main.box_device_.*
 import kotlinx.android.synthetic.main.box_device_detail.*
 import kotlinx.android.synthetic.main.box_title.*
-import kotlinx.android.synthetic.main.box_user.*
 import kotlinx.android.synthetic.main.box_user_detail.*
 import kotlinx.android.synthetic.main.dialog_no_found_item.*
 import kotlinx.android.synthetic.main.toolbar_detail.*
+import android.R.string.no
 
 
-class CustomerDetailActivity : AppCompatActivity() {
+
+
+
+class CustomerDetailActivity : AppCompatActivity(), View.OnTouchListener {
     private var scannedResult: String = ""
     private var userInfo: ArrayList<String> = ArrayList()
     private var getEditText: String? = null
@@ -52,16 +54,17 @@ class CustomerDetailActivity : AppCompatActivity() {
         initInquiryUserInformation()
         checkIntent()
         initRecordLocation()
+//      initSaveInfo()
         initScan()
         initBack()
-
     }
 
 
     private fun initScan() {
         scan.setOnClickListener {
             run {
-                IntentIntegrator(this).initiateScan();
+                IntentIntegrator(this)
+                    .initiateScan()
             }
         }
     }
@@ -100,10 +103,11 @@ class CustomerDetailActivity : AppCompatActivity() {
 
 
 
-    private fun clickDoneAction(){
+
+    private fun clickDoneAction() {
         var dbFromAssets = DBHelperJavaSimin(this)
         et_TerminalNumber.setOnEditorActionListener { v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_DONE){
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
                 getEditText = et_TerminalNumber.text.toString()
                 if (getEditText!!.isEmpty()) {
                     toast("شماره پایانه را وارد کنید")
@@ -116,12 +120,17 @@ class CustomerDetailActivity : AppCompatActivity() {
                         if (userInfo.size == 0 || userInfo.isEmpty()) {
                             showAlertDialog()
                         } else {
+                            rl_information.isFocusableInTouchMode = true
                             rl_scan.visibility = View.GONE
-                            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                            overridePendingTransition(
+                                android.R.anim.fade_in,
+                                android.R.anim.fade_out
+                            )
                             rl_information.visibility = View.VISIBLE
                             newUserInformation(userInfo)
 
-                            val imm = v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                            val imm =
+                                v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                             imm.hideSoftInputFromWindow(v.windowToken, 0)
                         }
                     }
@@ -177,21 +186,6 @@ class CustomerDetailActivity : AppCompatActivity() {
     }
 
 
-//    private fun newUserInformation(info: ArrayList<String>) {
-//        storeName.text = info[0]
-//        terminalNumber_title.text = info[1]
-//        customerName.text = info[2]
-//        phone_sabet.text = info[3]
-//        phone_hamrah.text = info[4]
-//        support_Tecnisiyan.text = info[5]
-//        terminalNumber_info.text = info[1]
-//        deviceModel.text = info[6]
-//        deviceSerial.text = info[7]
-//        rollNumber.text = info[8]
-//    }
-
-
-
     private fun checkIntent() {
         if (intent.extras != null) id = intent.getIntExtra("id_home", 0)
 
@@ -205,9 +199,9 @@ class CustomerDetailActivity : AppCompatActivity() {
 
     private fun changeSomeText(customer: Customer) {
         rl_scan.visibility = View.GONE
-        arrayOf(rl_information ).forEach { it.visibility = View.VISIBLE }
+        arrayOf(rl_information).forEach { it.visibility = View.VISIBLE }
         title_detail.text = "ویرایش مشتری"
-        tv_record_location.text = "ویرایش موقعیت"
+        tv_record_location.text = "ثبت اطلاعات"
         userInformationExist(customer!!)
         initWebView()
     }
@@ -219,30 +213,118 @@ class CustomerDetailActivity : AppCompatActivity() {
     }
 
 
+    private fun checkEdittext(et: EditText): String {
+        return et.text.toString().trim()
+    }
+
+
+    private fun getEditTextValue(): Boolean {
+        var b = DBHelperJavaSimin(this).getTerminal_number(et_terminalNumber.text.toString())
+        Log.e("444", "getEditTextValue: ${et_terminalNumber.text}")
+        if (checkEdittext(et_OwnerDevice).isEmpty() || checkEdittext(et_PhoneSabet).isEmpty()
+            || checkEdittext(et_PhoneHamrah).isEmpty() || checkEdittext(et_Address).isEmpty() ||
+            checkEdittext(et_SupportDevice).isEmpty() || checkEdittext(et_terminalNumber).isEmpty()
+            || checkEdittext(et_DeviceModel).isEmpty() || checkEdittext(et_DeviceSerial).isEmpty() ||
+            checkEdittext(et_RollNumber).isEmpty()
+        ) {
+            toast("همه فیلد ها را پرکنید")
+        } else if (et_PhoneSabet.text!!.length != 8) {
+            toast("تلفن ثابت وارد شده باید 8 کاراکتر باشد")
+        } else if (et_PhoneHamrah.length() != 11) {
+            toast("تلفن همراه باید 11 کاراکتر باشد")
+        } else if (b.size == 0) {
+            toast("شماره پایانه وارد شده نامعتبر می باشد")
+        } else return true
+        return false
+    }
+
+
+    private fun getAllEditTextValue(): Customer {
+        return Customer(
+            storeName.text.toString(),
+            et_terminalNumber.text.toString(),
+            et_OwnerDevice.text.toString(),
+            et_PhoneSabet.text.toString(),
+            et_PhoneHamrah.text.toString(),
+            et_Address.text.toString(),
+            et_SupportDevice.text.toString(),
+            et_DeviceModel.text.toString(),
+            et_DeviceSerial.text.toString(),
+            et_RollNumber.text.toString()
+        )
+    }
+
+
     private fun initRecordLocation() {
         btn_record_location.setOnClickListener {
-            if (checkLatLong(et_TerminalNumber.text.toString())) {
-                var intent = Intent(this, MapsActivity::class.java)
-                intent.putExtra("latitude", cm?.latitude)
-                intent.putExtra("longitude", cm?.longitude)//
-                intent.putExtra("id_customer_detail", cm!!.customerID)
-//                intent.putExtra("id_customer_detail", id)
-                startActivity(intent)
-            } else {
-                var i = Intent(this, MapsActivity::class.java)
-                i.putExtra("edittext_value", getEditText)
-                startActivity(i)
+            if (getEditTextValue()) {
+                if (checkLatLong(et_terminalNumber.text.toString())) {
+
+                    dao?.updateUserWithoutLatLong(getAllEditTextValue().terminalnNumer.toString() ,
+                getAllEditTextValue().deviceOwner.toString() ,getAllEditTextValue().phoneSabet.toString()  ,
+                getAllEditTextValue().phoneHamrah.toString() ,getAllEditTextValue().address.toString() ,
+                getAllEditTextValue().supportName.toString() ,getAllEditTextValue().deviceModel.toString() ,
+                getAllEditTextValue().deviceSerial.toString(),getAllEditTextValue().rollNumber.toString() ,
+                cm!!.customerID)
+                    finish()
+
+
+//                    var intent = Intent(this, MapsActivity::class.java)
+//                    intent.putExtra("latitude", cm?.latitude)
+//                    intent.putExtra("longitude", cm?.longitude)//
+//                    intent.putExtra("id_customer_detail", cm!!.customerID)
+//                    intent.putExtra("dd", Gson().toJson(getAllEditTextValue()))
+//                    Log.e("111", "initRecordLocation: ${cm!!.customerID}")
+//                    Log.e("111", "initRecordLocation: $id")
+//                    startActivity(intent)
+                } else {
+                    var i = Intent(applicationContext, MapsActivity::class.java)
+                    i.putExtra("edittext_value", getEditText)
+                    i.putExtra("dd", Gson().toJson(getAllEditTextValue()))
+                    startActivity(i)
+                }
             }
         }
     }
 
 
-    private fun initWebView(){
-        mWebView.visibility = View.VISIBLE
+//    private fun initSaveInfo(){
+//        tv_save_info.setOnClickListener {
+////            dao?.updateUserWithoutLatLong(getAllEditTextValue().terminalnNumer.toString() ,
+////                getAllEditTextValue().deviceOwner.toString() ,getAllEditTextValue().phoneSabet.toString()  ,
+////                getAllEditTextValue().phoneHamrah.toString() ,getAllEditTextValue().address.toString() ,
+////                getAllEditTextValue().supportName.toString() ,getAllEditTextValue().deviceModel.toString() ,
+////                getAllEditTextValue().deviceSerial.toString(),getAllEditTextValue().rollNumber.toString() ,
+////                id!!)
+//
+//            dao?.insert(Customer(storeName.text.toString(),
+//                getAllEditTextValue().terminalnNumer.toString() ,
+//                getAllEditTextValue().deviceOwner.toString() ,getAllEditTextValue().phoneSabet.toString()  ,
+//                getAllEditTextValue().phoneHamrah.toString() ,getAllEditTextValue().address.toString() ,
+//                getAllEditTextValue().supportName.toString() ,getAllEditTextValue().deviceModel.toString() ,
+//                getAllEditTextValue().deviceSerial.toString(),getAllEditTextValue().rollNumber.toString() ))
+//            finish()
+//        }
+//    }
+
+
+    private fun initWebView() {
+        linear_WebView.visibility = View.VISIBLE
         mWebView.webViewClient = WebViewClient()
-        mWebView.loadUrl("file:///android_asset/map.html");
+        mWebView.loadUrl("file:///android_asset/map.html")
         mWebView.settings.javaScriptEnabled = true
         mWebView.settings.setSupportZoom(true)
+        mWebView.setOnTouchListener(this)
+//        var client = object : WebViewClient() {
+//            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+//        toast("hello")
+//                return false
+//            }
+//        }
+//        mWebView.webViewClient = client;
+
+
+        
     }
 
 
@@ -269,6 +351,7 @@ class CustomerDetailActivity : AppCompatActivity() {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
+
 
 
     private fun toast(text: String) {
@@ -298,11 +381,25 @@ class CustomerDetailActivity : AppCompatActivity() {
 //    }
 
 
-
-
-
     private fun initBack() {
         img_back.setOnClickListener { finish() }
+    }
+
+    override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+        if (p0 == mWebView && p1!!.action == MotionEvent.ACTION_DOWN) {
+            toast("hello")
+            cm = dao!!.checkTerminal(et_terminalNumber.text.toString())
+            var intent = Intent(this, MapsActivity::class.java)
+                 intent.putExtra("latitude", cm?.latitude)
+                 intent.putExtra("longitude", cm?.longitude)
+                 intent.putExtra("id_customer_detail", cm!!.customerID)
+//                    intent.putExtra("dd", Gson().toJson(getAllEditTextValue()))
+                 Log.e("111", "onTouch : ${cm!!.customerID}")
+                 Log.e("111", "onTouch: ${cm!!.latitude}")
+                 Log.e("111", "onTouch: ${cm!!.longitude}")
+                 startActivity(intent)
+        }
+        return false
     }
 
 
