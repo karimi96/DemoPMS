fed638e69d69691644de46d183f8a652c0fc208c
karimi <zmanyamyd5@gmail.com>
2022-07-12 15:46:58 +0430

    Add Query Of Update

--- app/src/main/java/com/karimi/googlemap/database/dao/CustomerDao.kt
+++ app/src/main/java/com/karimi/googlemap/database/dao/CustomerDao.kt
@@ -18,9 +18,54 @@ interface CustomerDao {
 //    @Query("SELECT latitude,longitude from customer_table where customerID = :id")
 //    fun getLatLong(id: Int) :List<String>
 
+
     @Query("UPDATE customer_table SET latitude = :lat ,longitude = :longitude where customerID = :id")
     fun updateLatLong(lat : Double? , longitude: Double? , id: Int)
 
+
+
+
+
+//    @Query("UPDATE customer_table SET " +
+//            "terminal_num = :terminal , " +
+//            "device_owner = :deviceOwner , " +
+//            "phone_sabet = :pSabet ," +
+//            "phone_hamrah = :pHamrah ," +
+//            "address = :address ," +
+//            "support_name = :supportName ," +
+//            "device_model = :dModel ," +
+//            "device_Serial = :dSerial ," +
+//            "roll_num = :roll , " +
+//            "latitude = :lat ," +
+//            "longitude = :longitude where " +
+//            "customerID = :id")
+//    fun updateLatLong(terminal: String ,deviceOwner: String ,pSabet: String ,
+//                      pHamrah: String ,address: String ,supportName: String ,
+//                      dModel: String ,dSerial: String ,roll: String ,
+//                      lat : Double? , longitude: Double? , id: Int)
+
+
+
+    @Query("UPDATE customer_table SET " +
+            "terminal_num = :terminal , " +
+            "device_owner = :deviceOwner , " +
+            "phone_sabet = :pSabet ," +
+            "phone_hamrah = :pHamrah ," +
+            "address = :address ," +
+            "support_name = :supportName ," +
+            "device_model = :dModel ," +
+            "device_Serial = :dSerial ," +
+            "roll_num = :roll , " +
+            "customerID = :id")
+    fun updateUserWithoutLatLong(terminal: String ,deviceOwner: String ,pSabet: String ,
+                      pHamrah: String ,address: String ,supportName: String ,
+                      dModel: String ,dSerial: String ,roll: String ,id: Int)
+
+
+//    @Query("INSERT INTO customer_table VALUES(null,:a,:b)")
+//    fun getall(a: String?, b: String?, columna: String?, columnb: String?)
+//
+//
 
     @Insert
     fun insert(customer: Customer)
